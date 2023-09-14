e312ab68af6859584ce8ffe984d8d5f228db8a4a
dennischengsc <dennischeng520@gmail.com>
2023-09-14 17:50:17 +0800

    updated predicton.py

--- prediction.py
+++ prediction.py
@@ -31,34 +31,27 @@ def make_predictions(best_model, reference_image_dir, output_dir):
         9: 'vehicle'
     }
 
-    # Define the class IDs you want to capture
-    target_class_ids = [4, 5, 3]
+    result = results[0]
+    box = result.boxes[0]
+    print("Object type:",box.cls[0])
+    print("Coordinates:",box.xyxy[0])
+    print("Probability:",box.conf[0])
 
-    # Create the output directory if it doesn't exist
-    os.makedirs(output_dir, exist_ok=True)
+    cords = box.xyxy[0].tolist()
+    class_id = box.cls[0].item()
+    conf = box.conf[0].item()
+    print("Object type:", class_id)
+    print("Coordinates:", cords)
+    print("Probability:", conf)
+    print(result.names)
 
-    # Now you can iterate through the filtered results and work with them
-    for result in results:
-        image = cv2.imread(os.path.join(reference_image_dir, result["image_path"]))  # Load the image using OpenCV
-        boxes = result.boxes
-        masks = result.masks
-        keypoints = result.keypoints
-        probs = result.probs
 
-        # Iterate through detected objects and draw bounding boxes and labels for filtered classes
-        for box, cls, prob in zip(boxes, boxes.cls, probs):
-            if int(cls) in target_class_ids:
-                class_name = classes[int(cls)]
-                score = f"{class_name}: {prob:.2f}"
-                x, y, w, h = box.xyxy[0].tolist()
-                x, y, w, h = int(x), int(y), int(w), int(h)
-                color = (0, 255, 0)  # Green color for bounding boxes
-                cv2.rectangle(image, (x, y), (w, h), color, 2)
-                cv2.putText(image, score, (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
-
-        # Save the modified image with only filtered classes
-        image_name = os.path.basename(result["image_path"])
-        modified_image_path = os.path.join(output_dir, f"filtered_{image_name}")
-        cv2.imwrite(modified_image_path, image)
-
-        print(f"Filtered image saved at {modified_image_path}")
+    for box in result.boxes:
+        class_id = result.names[box.cls[0].item()]
+        cords = box.xyxy[0].tolist()
+        cords = [round(x) for x in cords]
+        conf = round(box.conf[0].item(), 2)
+        print("Object type:", class_id)
+        print("Coordinates:", cords)
+        print("Probability:", conf)
+        print("---")
