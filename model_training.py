a88cb4ac086d281990229c585be4cbdb4bb3b12a
dennischengsc <dennischeng520@gmail.com>
2023-09-17 13:49:21 +0800

    live predcution and streamlit first version

--- model_training.py
+++ model_training.py
@@ -1,7 +1,7 @@
 from ultralytics import YOLO
 import os
 
-def train_yolo_model(work_dir, num_classes, img_size=640, epochs=3, batch=16):
+def train_yolo_model(work_dir, num_classes, img_size=640, epochs=200, batch=32, save_period=10):
     model = YOLO(os.path.join(work_dir, 'yolov8n.pt'))
     model.train(
         data=os.path.join(work_dir, 'data.yaml'),
@@ -10,5 +10,7 @@ def train_yolo_model(work_dir, num_classes, img_size=640, epochs=3, batch=16):
         epochs=epochs,
         batch=batch,
         mode='train',
-        name='yolov8n_training_result'
+        name='yolov8n_change_path_test',
+        resume=False,
+        save_period=save_period
     )
