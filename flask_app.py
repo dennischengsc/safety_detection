591f1a753714ab9be3746a0e01baedb9dac48db7
dennischengsc <dennischeng520@gmail.com>
2023-09-17 22:23:56 +0800

    try for streamlit deploy

---
+++ flask_app.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template
+import subprocess
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/run_streamlit')
+def run_streamlit():
+    try:
+        subprocess.Popen(['streamlit', 'run', 'app.py'])  # Replace 'app.py' with your Streamlit app filename
+        return 'Streamlit app is running!'
+    except Exception as e:
+        return f'Error: {str(e)}'
+
+if __name__ == '__main__':
+    app.run(debug=True)
