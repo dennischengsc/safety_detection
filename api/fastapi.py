e33afb5c45e9513aacdbe70f5ed719833f76cc39
zafz ng <unspecificjoker@gmail.com>
2023-09-20 10:19:59 +0800

    reupdate of fastapi file

--- api/fastapi.py
+++ api/fastapi.py
@@ -21,18 +21,22 @@ app.add_middleware(
 #model
 model= YOLO(LOCAL_MODEL_PATH)
 #Setting up detection function for images
-@app.post("/detect_image/")
 async def detect_image(image_upload: UploadFile = File(...)):
     if image_upload:
         # IMPT** need to read uploaded images as bytes
         image_bytes= await image_upload.read()
-                # Convert the bytes to an OpenCV image
+        # Convert the bytes to an OpenCV image
         image = cv2.imdecode(np.frombuffer(image_bytes, np.uint8), -1)
         # Perform object detection using the YOLOv8 model
-        results = model.predict(image)
-
-        # # Convert images back to bytes
-        # _, image_encoded= cv2.imdecode(".jpg", image_with_detections)
-        # image_bytes = image_encoded.tobytes()
-        return results
-    raise HTTPException(status_code=400, detail="No image provided")
+        res = model.predict(image)
+        # st.write(res)
+        # boxes = res[0].boxes
+        res_plotted = res[0].plot()[:, :, ::-1]
+        bgr_image = cv2.cvtColor(res_plotted, cv2.COLOR_RGB2BGR)
+        # Encode the image as JPEG (you can use other formats like PNG)
+        _, image_encoded = cv2.imencode(".jpg",bgr_image)
+        # Convert the image to bytes
+        image_results = image_encoded.tobytes()
+        # Return the image as a response
+        return StreamingResponse(io.BytesIO(image_results), media_type="image/jpeg")
+    raise HTTPException(status_code=400, detail="No image provided")
